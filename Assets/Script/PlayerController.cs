2e5f7cadaedfcc0aa6320c5b80bcdf0309dbc211
RazOre1st <88528720+RazOre1st@users.noreply.github.com>
2021-10-01 05:03:17 -0700

    Add files via upload

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
-
+	public GameObject Projectile;
+	public Vector2 projectileVelocity;
+	public Vector2 projectileOffset;
+	public float cooldown = 0.5f; 
+	bool isCanShoot = true;
 	bool isJump = true;
 	bool isDead = false;
 	int idMove = 0;
@@ -14,47 +18,80 @@ public class PlayerController : MonoBehaviour {
 	{
 		anim = GetComponent<Animator>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.LeftArrow))
-		{
-			MoveLeft();
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			MoveLeft ();
 		}
-		if (Input.GetKeyDown(KeyCode.RightArrow))
-		{
-			MoveRight();
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			MoveRight ();
 		}
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			Jump();
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Jump ();
 		}
-		if (Input.GetKeyUp(KeyCode.RightArrow))
-		{
+		if (Input.GetKeyUp (KeyCode.RightArrow)) {
 			Idle ();
 		}
-		if (Input.GetKeyUp(KeyCode.LeftArrow))
-		{
+		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
 			Idle ();
 		}
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			Fire ();
+		}
+
 		Move ();
 		Dead ();
-	}
+		}
+		void Fire()
+		{
+		if (isCanShoot)
+		{
+		GameObject bullet = Instantiate(Projectile, (Vector2)transform.position - projectileOffset * transform.localScale.x, Quaternion.identity);
 
+ 		Vector2 velocity = new Vector2(projectileVelocity.x * transform.localScale.x, projectileVelocity.y);
+		bullet.GetComponent<Rigidbody2D>().velocity = velocity * -1 ;
+		
+		Vector3 scale = transform.localScale;
+		bullet.transform.localScale = scale * -1;
+		
+ 		StartCoroutine(CanShoot());
+ 		anim.SetTrigger("shoot");
+		}
+	}
+		IEnumerator CanShoot()
+	 	{
+		 anim.SetTrigger("shoot");
+		 isCanShoot = false;
+		 yield return new WaitForSeconds(cooldown);
+			isCanShoot = true;
+		}
+
+		private void OnCollisionEnter2D(Collision2D collision)
+	 	{
+		 if (collision.transform.tag.Equals("Peluru"))
+			 {
+			 isCanShoot = true;
+			 }
+		 if (collision.transform.tag.Equals("Enemy"))
+			{	 
+			isDead = true;
+			}
+		 }
+		
 	private void OnCollisionStay2D(Collision2D collision)
 	{
-		// Kondisi ketika menyentuh tanah
+		
 		 if (isJump)
 			 {
 			 anim.ResetTrigger("jump");
 			 if (idMove == 0) anim.SetTrigger("idle");
 			 isJump = false;
 			 }
-		}
+	}
 
 	 private void OnCollisionExit2D(Collision2D collision)
 	 {
-		 // Kondisi ketika menyentuh tanah
 		 anim.SetTrigger("jump");
 		 anim.ResetTrigger("run");
 		 anim.ResetTrigger("idle");
@@ -64,7 +101,7 @@ public class PlayerController : MonoBehaviour {
 	 public void MoveRight()
 		 {
 		 idMove = 1;
-	 	}
+	 	 }
 
 	 public void MoveLeft()
 		{
@@ -75,7 +112,6 @@ public class PlayerController : MonoBehaviour {
 		{
 		if (idMove == 1 && !isDead)
 		{
-		 // Kondisi ketika bergerak ke kekanan
 	 	if (!isJump) anim.SetTrigger("run");
 			transform.Translate (1 * Time.deltaTime * 5f, 0, 0);
 				
@@ -84,7 +120,6 @@ public class PlayerController : MonoBehaviour {
 			}
 		 	if (idMove == 2 && !isDead)
 		 	{
-		 	// Kondisi ketika bergerak ke kiri
 			if (!isJump) anim.SetTrigger("run");
 			 transform.Translate(-1 * Time.deltaTime * 5f, 0, 0);
 			transform.localScale = new Vector3(1f, 1f, 1f);
@@ -92,27 +127,24 @@ public class PlayerController : MonoBehaviour {
 		}
 
 	 public void Jump()
-	{
+		{
 		 if (!isJump)
 			 {
-			 // Kondisi ketika Loncat
 			gameObject.GetComponent<Rigidbody2D>().AddForce
 			(Vector2.up * 300f);
 			 }
 		}
 
 	 private void OnTriggerEnter2D(Collider2D collision)
-	 {
+	 	{
 		 if (collision.transform.tag.Equals("Coin"))
 			 {
-			 //Data.score += 15;
 			 Destroy(collision.gameObject);
 			 }
 		 }
 
 	 public void Idle()
-	 {
-		 // kondisi ketika idle/diam
+	{
 		 if (!isJump)
 			 {
 			 anim.ResetTrigger("jump");
@@ -128,7 +160,6 @@ public class PlayerController : MonoBehaviour {
 	 {
 			 if (transform.position.y < -10f)
 				 {
-				 // kondisi ketika jatuh
 				 isDead = true;
 			 }
 		 }
---
+++ Assets/Script/QuitOrExit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitOrExit : MonoBehaviour {
+	public string NameScene = "Gameplay2";
+	private void Update()
+	{
+		if (Input.GetKeyUp (KeyCode.Escape)) {
+			Application.Quit ();
+		}
+	}
+
+	private void OnTriggerEnter2D (Collider2D collision)
+	{
+		if (collision.tag.Equals ("Player")) {
+			SceneManager.LoadScene ("Gameplay2");
+		}
+	}
+
+	void Start () {
+		
+	}
+}
+	
